34ef5ce734a49cae957ce41f02bcabcd4f32c781
Josef Leventon <josef.leventon@gmail.com>
2023-03-07 15:57:17 -0500

    Prune unused & fix provider naming

--- client/react/client/SparkProvider.tsx
+++ client/react/client/SparkProvider.tsx
@@ -1,48 +1,45 @@
-import { useCallback, useEffect, useMemo, useState } from 'react'
-import { SparkClient } from 'client/core'
-import SparkClientContext from './SparkClient'
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import { SparkClient } from 'client/core';
+import SparkClientContext from './SparkClient';
 
-import { CONTRACT_ADDRESS } from 'util/constants'
-import useWallet from '../wallet/useWallet'
+import { CONTRACT_ADDRESS, CW20_ADDRESS } from 'util/constants';
+import useWallet from '../wallet/useWallet';
 
-export default function StargazeProvider({
-  children,
-}: {
-  children: JSX.Element
-}) {
-  const [, updateState] = useState<{}>()
-  const forceUpdate = useCallback(() => updateState({}), [])
+export default function SparkProvider({ children }: { children: JSX.Element }) {
+  const [, updateState] = useState<{}>();
+  const forceUpdate = useCallback(() => updateState({}), []);
 
-  const { wallet, signingCosmWasmClient } = useWallet()
+  const { wallet, signingCosmWasmClient } = useWallet();
 
   const client = useMemo(
     () =>
       new SparkClient({
         wallet: wallet || null,
         fundingContract: CONTRACT_ADDRESS,
-        signingCosmWasmClient: signingCosmWasmClient || null,
+        cw20Contract: CW20_ADDRESS,
+        signingCosmWasmClient: signingCosmWasmClient || null
       }),
-    [wallet, signingCosmWasmClient],
-  )
+    [wallet, signingCosmWasmClient]
+  );
 
   // Connect client
   useEffect(() => {
     // Unsigned Client
     async function connectClient() {
-      await client?.connect()
-      forceUpdate()
+      await client?.connect();
+      forceUpdate();
     }
 
-    connectClient()
-  }, [client, forceUpdate])
+    connectClient();
+  }, [client, forceUpdate]);
 
   return (
     <SparkClientContext.Provider
       value={{
-        client,
+        client
       }}
     >
       {children}
     </SparkClientContext.Provider>
-  )
+  );
 }
--- util/constants.ts
+++ util/constants.ts
@@ -1,4 +1,4 @@
-export const microAmountMultiplier = 1_000_000
+export const microAmountMultiplier = 1_000_000;
 
-export const CONTRACT_ADDRESS = process.env
-  .NEXT_PUBLIC_FUNDING_CONTRACT_ADDRESS!
+export const CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_FUNDING_CONTRACT_ADDRESS!;
+export const CW20_ADDRESS = process.env.NEXT_PUBLIC_CW20_CONTRACT_ADDRESS!;
